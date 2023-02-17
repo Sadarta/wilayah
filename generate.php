c41f21a418b2c51a68a2458a6941f5539e5ca2ce
Lerian Febriana, A.Md.Kom <67350647+kanglerian@users.noreply.github.com>
2023-02-17 10:49:12 +0700

    Update generate.php

--- generate.php
+++ generate.php
@@ -5,6 +5,8 @@ use Emsifa\ApiWilayah\Repository;
 
 require "vendor/autoload.php";
 
+header('Access-Control-Allow-Origin: http://localhost:3001');
+
 $repository = new Repository(__DIR__.'/data');
 
 $repository->cache('districts.csv');
